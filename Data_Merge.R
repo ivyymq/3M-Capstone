e2a8a023f8ea7090e7af146db348ad66190d8951
ivyymq <ivyymq@gmail.com>
2015-02-19 22:03:43 -0500

    Data_Merge_20150215
    Add the data dictionary merging part

--- Data_Merge.R
+++ Data_Merge.R
@@ -1,6 +1,6 @@
 # title: Data_Merge
 # author: 3M Capstone Team: Ivy Yang
-# version: 2.0
+# version: 3.0
 # output: .csv document
 
 # install.packages("reshape")
@@ -11,24 +11,19 @@ HAC.pro <- read.csv("HOSPITAL_QUARTERLY_HAC_DOMAIN_HOSPITAL_11_24_2014 - pro.csv
 HAI.pro <- read.csv("Healthcare Associated Infections - Hospital - pro.csv", header = T, colClasses = "character")
 HCAHPS.pro <- read.csv("HCAHPS - Hospital - pro.csv", header = T, colClasses = "character")
 RCD.pro <- read.csv("Readmissions Complications and Deaths - Hospital - pro.csv", header = T, colClasses = "character")
+# merge processed data
+data.pro <- merge_recurse(list(HAC.pro, HAI.pro, HCAHPS.pro, RCD.pro))
+data.pro <- arrange(data.pro, Provider.ID)
+# output merged processed data as .csv file
+write.csv(data.pro, "Data_pro.csv", row.names = FALSE)
+
 
 # load data dictionary data
 HAC.dic <- read.csv("HOSPITAL_QUARTERLY_HAC_DOMAIN_HOSPITAL_11_24_2014 - dic.csv", header = T, colClasses = "character")
 HAI.dic <- read.csv("Healthcare Associated Infections - Hospital - dic.csv", header = T, colClasses = "character")
 HCAHPS.dic <- read.csv("HCAHPS - Hospital - dic.csv", header = T, colClasses = "character")
 RCD.dic <- read.csv("Readmissions Complications and Deaths - Hospital - dic.csv", header = T, colClasses = "character")
-
-# merge processed data
-data.pro <- merge_recurse(list(HAC.pro, HAI.pro, HCAHPS.pro, RCD.pro))
-data.pro <- arrange(data.pro, Provider.ID)
-
 # merge data dictionary
 data.dic <- merge_recurse(list(HAC.dic, HAI.dic, HCAHPS.dic, RCD.dic))
-
-# output merged processed data as .csv file
-write.csv(data.pro, "Data_pro.csv", row.names = FALSE)
 # output merged data dictionary as .csv file
 write.csv(data.dic, "Data_dic.csv", row.names = FALSE)
-
-# replace "Not Available" with NA in future process
-# data.merged[data.merged=="Not Available"] = NA
