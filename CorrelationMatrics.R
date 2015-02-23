460030f13b315a60334816d84afc11e137b3fb28
xcy0182 <congyu.xu@gmail.com>
2015-02-22 22:12:21 -0500

    CorrelationMatrics_20150222

--- CorrelationMatrics.R
+++ CorrelationMatrics.R
@@ -11,7 +11,9 @@ hospital.data = read.csv(file = "Data_Pro_WeightedSurvey_Infection.csv", header 
 correlation.value = cor(hospital.data, use="complete.obs")
 write.csv(correlation.value, "correlation_infection_survey.csv")  #Output correlation table
 
-#plot heatmap of correlation
+
+
+#plot heatmap of all correlation
 library(corrplot)
 jpeg("correlation_infection_survey_circle.jpg", width = 1800, height = 1800,quality = 100)
 corrplot(correlation.value, method = "circle")
@@ -22,6 +24,41 @@ corrplot(correlation.value, method = "number")
 dev.off()
 
 
+#Get only cross Correlation matrics-survey against infection
+correlation.value.cross = cor(hospital.data[, 1:37], hospital.data[,38:48],use="complete.obs")
+write.csv(correlation.value.cross, "correlation_cross.csv")
+library(corrplot)
+jpeg("correlation_infection_cross_circle.jpg", width = 1800, height = 1800,quality = 100)
+corrplot(correlation.value.cross, method = "circle")
+dev.off()
+
+jpeg("correlation_infection_cross_number.jpg", width = 1800, height = 1800, quality = 100)
+corrplot(correlation.value.cross, method = "number")
+dev.off()
+
+#Get only inner Correlation matrics of infection 
+correlation.value.infection = cor(hospital.data[, 1:37],use="complete.obs")
+write.csv(correlation.value.infection, "correlation_infection.csv")
+library(corrplot)
+jpeg("correlation_infection_circle.jpg", width = 1800, height = 1800,quality = 100)
+corrplot(correlation.value.infection, method = "circle")
+dev.off()
+
+jpeg("correlation_infection_number.jpg", width = 1800, height = 1800, quality = 100)
+corrplot(correlation.value.infection, method = "number")
+dev.off()
+
+#Get only inner Correlation matrics of survey 
+correlation.value.survey = cor(hospital.data[, 38:48],use="complete.obs")
+write.csv(correlation.value.survey, "correlation_survey.csv")
+library(corrplot)
+jpeg("correlation_survey_circle.jpg", width = 1800, height = 1800,quality = 100)
+corrplot(correlation.value.survey, method = "circle")
+dev.off()
+
+jpeg("correlation_survey_number.jpg", width = 1800, height = 1800, quality = 100)
+corrplot(correlation.value.survey, method = "number")
+dev.off()
 
 
 
